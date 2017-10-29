4c51235d2391481cbea0297249f7ac0823b663c3
M.Monem <mmonem@gmail.com>
2017-10-29 21:28:38 +0200

    Users admin module - step #1

---
+++ UsersAdminModule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\modules\usersadmin;
+
+use yii\base\Module;
+
+/**
+ * Description of NewsModule
+ *
+ * @author monem
+ */
+class UsersAdminModule extends Module
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
+
+?>
---
+++ views/users/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\User */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'uuid')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'auth_key')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
